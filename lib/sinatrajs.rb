1e3f1f9c90d5ec3b3b93de5b16e92e7a46b4e4db
Jain Waldrip <jkwaldri@iu.edu>
2015-12-16 11:48:08 -0500

    Serve static page with Bootstrap & jQuery

--- lib/sinatrajs.rb
+++ lib/sinatrajs.rb
@@ -5,8 +5,12 @@ require_relative "sinatrajs/version"
 
 module Sinatrajs
   class App < Sinatra::Base
+    set :root, File.expand_path("#{File.dirname(__FILE__)}/../")
+
+    enable :static
+
     get '/' do
-      'Hello, World!'
+      redirect '/index.html'
     end
   end
 end
